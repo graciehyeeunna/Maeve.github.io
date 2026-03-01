425edec1fb425335b3f4c56c750d247c64aeb4cf
chaerinKim <chae-rin.kim@pwc.com>
2026-03-01 21:19:17 +0900

    fix: https://maeve-hyeeunna.github.io 로 도메인 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // GitHub Pages 루트 도메인(https://Maeve.github.io/) 배포를 위해 경로 설정을 제거합니다.
+  // GitHub Pages 루트 도메인(https://maeve-hyeeunna.github.io/) 배포를 위해 경로 설정을 제거합니다.
   basePath: "",
   assetPrefix: "",
   trailingSlash: true,
